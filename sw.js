cbbb8dc07eefbe691db061506e43cdebd41d8a48
tawfiqur <47170524+tawfiqur@users.noreply.github.com>
2020-07-01 17:47:18 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+var C = ['Cache-V'];
+var E = ['Cache-V'];
+var U = [
+'/manifest.json',
+'/index.html',
+
+//Service Worker installing & creating a cache whih predefined list above (on VAR U = ....)
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C)
+  .then(function(cache) {
+     return cache.addAll(U);
+     })
+  );
+});
+
+// Service Worker activating & if there is any previous caches which is not necessary will be removed.
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys()
+  .then(C => {
+      return Promise.all(C.map(C => {
+      if (!E.includes(C)) {
+        return caches.delete(C);
+  }}));}));});
+
+//In every page request, Service Worker will try to find a match in cache, if match mound then it will serve from cache or else will fetch from network.  
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request)
+  .then(function(r) {
+  if (r) {return r;}
+  return fetch(e.request);
+}));});
